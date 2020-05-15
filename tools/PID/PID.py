c1f773fc91f4718d85cde5236ae8926727ba1073
Kajetan Jonak <kajetan@jonak.org.pl>
2020-05-15 22:06:51 +0200

    added lock and removed time checking

--- tools/PID/PID.py
+++ tools/PID/PID.py
@@ -3,7 +3,7 @@ import time, threading
 
 class PID:
 
-    def __init__(self, P=0., I=0., D=0., set_point = 0, sample_time = 0.01):
+    def __init__(self, P=0., I=0., D=0., set_point = 0, sample_time = 0.001):
 
         self.Kp = P
         self.Ki = I
@@ -31,41 +31,34 @@ class PID:
 
         self.max_output = 0
 
+
+
     def update(self, feedback):
 
         error = self.set_point - feedback
-
+      #po co sprawdzać czas skoro pętla ma określoną częstotliwość?
         self.current_time = time.time()
 
         delta_time = self.current_time - self.last_time
         delta_error = error - self.last_error
-
-        if (delta_time >= self.sample_time):
-
-            self.PTerm = error
-            self.ITerm += error * delta_time
-
-            if (self.ITerm > self.windup_guard):
-                self.ITerm = self.windup_guard
-            elif (self.ITerm < -self.windup_guard):
-                self.ITerm = -self.windup_guard
-
-            self.DTerm = delta_error / delta_time
-
-            #save last data for next calculation
-            self.last_time = self.current_time
-            self.last_error = error
-            
+        self.PTerm = error
+        self.ITerm += error * delta_time
+        if (self.ITerm > self.windup_guard):
+            self.ITerm = self.windup_guard
+        elif (self.ITerm < -self.windup_guard):
+            self.ITerm = -self.windup_guard
+        self.DTerm = delta_error / delta_time
+        #save last data for next calculation
+        self.last_time = self.current_time
+        self.last_error = error
+        with self.lock:
             self.output = self.Kp * self.PTerm + self.Ki * self.ITerm + self.Kd * self.DTerm
+        # na testy, zeby predkosc sie nie zwiekszyla za bardzo
+        if self.output > self.max_output:
+            self.output = self.max_output
 
-            # na testy, zeby predkosc sie nie zwiekszyla za bardzo
-            if self.output > self.max_output:
-                self.output = self.max_output
+        return self.output
 
-            return self.output
-
-        else:
-            return 0  #self.output_prev
 
     def setKp(self, Kp):
         self.Kp = Kp
